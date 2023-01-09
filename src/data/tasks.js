ee5007d7ccfc90262469452e9162375fee23b888
kevwil <108852395+sckayelpro@users.noreply.github.com>
2023-01-08 22:58:16 -0400

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'my first task',
+        description: 'my first task'
+    },
+    {
+        id: 1,
+        title: 'my second task',
+        description: 'my second task'
+    },
+    {
+        id: 2,
+        title: 'my third task',
+        description: 'my third task'
+    },
+];
